c4f28f79318f5f501f32b12611f52945b763e1bd
dnauu-bmsotc <87213021+dnauu-bmsotc@users.noreply.github.com>
2026-08-12 19:34:52 +0300

    added parser

---
+++ server/src/schema/ast.ts
@@ -0,0 +1,107 @@
+import { Diagnostic, DiagnosticSeverity, Position, Range } from 'vscode-languageserver';
+
+interface AST {
+	elements: ASTElement[];
+}
+
+interface ASTElement {
+	name: string;
+	elementType: string;
+	fields: ASTField[];
+	range: Range;
+}
+
+interface ASTField {
+	name: string;
+	values: ASTValue[];
+	range: Range;
+}
+
+interface ASTValue {
+	text: string;
+	range: Range;
+}
+
+interface ASTParseResult {
+	AST: AST;
+	diagnostics: Diagnostic[];
+}
+
+export function parseIntoAST(text: string): ASTParseResult {
+    const lines = text.split(/\r?\n/);
+	const elements: ASTElement[] = [];
+	let current: ASTElement | null = null;
+	const diagnostics: Diagnostic[] = [];
+
+	function pushDiagnostic(message: string, start: Position, end: Position) {
+		diagnostics.push({
+			severity: DiagnosticSeverity.Error,
+			range: { start: start, end: end},
+			message: message,
+		});
+	}
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+		const lineStartPos = { line: i, character: 0 };
+		const lineEndPos = { line: i, character: line.length };
+
+		if (line.startsWith('element_start')) {
+			if (current) {
+				pushDiagnostic("Missing element_end", lineStartPos, lineEndPos);
+			}
+			const parts = line.replace(/,+$/, "").split(',');
+			if (parts.length >= 3) {
+				current = {
+					name: parts[1],
+					elementType: parts[2],
+					fields: [],
+					range: Range.create(lineStartPos, lineEndPos),
+				};
+			}
+			else {
+				pushDiagnostic("Incomplete element definition", lineStartPos, lineEndPos);
+			}
+		}
+		else if (line.startsWith('element_end')) {
+			if (current) {
+				elements.push(current);
+				current = null;
+			}
+			else {
+				pushDiagnostic("Missing element_start", lineStartPos, lineEndPos);
+			}
+		}
+		else {
+			if (current) {
+				const parts = [...text.matchAll(/[^,]+/g)].map(match => ({
+					value: match[0],
+					start: { line: i, character: match.index },
+					end: { line: i, character: match.index + match[0].length},
+				}));
+				if (parts.length >= 1) {
+					current.fields.push({
+						name: parts[0].value,
+						values: [],
+						range: Range.create(parts[0].start, parts[0].end),
+					});
+				}
+				for (let i = 1; i < parts.length; i++) {
+					current.fields[current.fields.length - 1].values.push({
+						text: parts[i].value,
+						range: Range.create(parts[i].start, parts[i].end),
+					});
+				}
+			}
+			else {
+				if (line.trim()) {
+					pushDiagnostic("Missing element_start", lineStartPos, lineEndPos);
+				}
+			}
+		}
+    }
+	return {
+		AST: { elements },
+		diagnostics: diagnostics,
+	};
+}
