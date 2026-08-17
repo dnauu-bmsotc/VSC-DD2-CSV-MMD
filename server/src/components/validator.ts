08f2c56ec04944af3c2f63f4c8038e545777e165
dnauu-bmsotc <87213021+dnauu-bmsotc@users.noreply.github.com>
2026-08-17 13:11:44 +0300

    input bool int float validation

--- server/src/components/validator.ts
+++ server/src/components/validator.ts
@@ -1,8 +1,9 @@
-import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver';
+import { Diagnostic, DiagnosticSeverity, Range } from 'vscode-languageserver';
 import { DD2CSVMMDSettings } from './configuration';
-import { AST } from './parser';
+import { AST, ASTField, ASTValue } from './parser';
 import { CompiledData } from './compiler';
 import { Index } from './indexer';
+import { TypeDefinition } from './schema';
 
 export function validateAstBySchema(
 	ast: AST,
@@ -29,8 +30,10 @@ export function validateAstBySchema(
 			}
 			continue;
 		}
+		const elementDefinition = compiledData.schema[element.elementType];
 		for (const field of element.fields) {
-			if (!(field.name in compiledData.schema[element.elementType].fields)) {
+			const fieldDefinition = elementDefinition.fields[field.name];
+			if (!fieldDefinition) {
 				if (configuration.validateFieldNames) {
 					diagnostics.push({
 						severity: DiagnosticSeverity.Error,
@@ -39,10 +42,99 @@ export function validateAstBySchema(
 					});
 				}
 			}
-			// validateFieldInput();
+			if (configuration.validateFieldInput) {
+				const diagnostic = validateInput(field, field.values, fieldDefinition.input, compiledData, astIndex);
+				diagnostic && diagnostics.push(diagnostic);
+			}
 		}
 	}
 	
 	console.log(`Validation: ${(performance.now() - t0).toFixed(1)} ms`);
 	return diagnostics;
+}
+
+function validateInput(field: ASTField, values: ASTValue[], definition: TypeDefinition, compiledData: CompiledData, astIndex: Index): Diagnostic | null {
+	switch (definition.type) {
+		case "any":
+			return null;
+
+		case "bool":
+			if (values.length === 0) {
+				return createExpectedTypeDiagnostic("boolean", field.range);
+			}
+			else {
+				const content = values[0].text;
+				if ((content !== "True") && (content !== "False")) {
+					return createExpectedTypeDiagnostic("boolean", values[0].range);
+				}
+				if (values.length > 1) {
+					console.log(values)
+					return createExpectedEndOfInputDiagnostic(values.slice(1));
+				}
+				return null;
+			}
+
+		case "int":
+			if (values.length === 0) {
+				return createExpectedTypeDiagnostic("integer", field.range);
+			}
+			else {
+				const content = values[0].text;
+				if (!isIntegerString(content)) {
+					return createExpectedTypeDiagnostic("integer", values[0].range);
+				}
+				if (values.length > 1) {
+					console.log(values)
+					return createExpectedEndOfInputDiagnostic(values.slice(1));
+				}
+				return null;
+			}
+
+		case "float":
+			if (values.length === 0) {
+				return createExpectedTypeDiagnostic("float", field.range);
+			}
+			else {
+				const content = values[0].text;
+				if (!isNumericString(content)) {
+					return createExpectedTypeDiagnostic("float", values[0].range);
+				}
+				if (values.length > 1) {
+					console.log(values)
+					return createExpectedEndOfInputDiagnostic(values.slice(1));
+				}
+				return null;
+			}
+	
+		default:
+			// console.log(`Unknown input type: ${definition.type}`)
+			return null;
+	}
+}
+
+const isIntegerString = (str: string) => /^-?\d+$/.test(str);
+
+const isNumericString = (str: string) => !isNaN(Number(str));
+
+function createExpectedTypeDiagnostic(expectedType: string, range: Range): Diagnostic {
+	return {
+		severity: DiagnosticSeverity.Error,
+		range: range,
+		message: `Expected type: ${expectedType}`
+	};
+}
+
+function createExpectedEndOfInputDiagnostic(values: ASTValue[]): Diagnostic | null {
+	if (!values.length) {
+		return null;
+	}
+	const range = Range.create(
+		{ line: values[0].range.start.line, character: values[0].range.start.character },
+		{ line: values[0].range.end.line, character: values[values.length - 1].range.end.character },
+	);
+	return {
+		severity: DiagnosticSeverity.Error,
+		range: range,
+		message: `Expected end of input`
+	};
 }
