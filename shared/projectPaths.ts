d782b6caed7849ed4f097ab8476ef673701935ca
dnauu-bmsotc <87213021+dnauu-bmsotc@users.noreply.github.com>
2026-09-16 21:55:47 +0300

    switched to flat ods files

--- shared/projectPaths.ts
+++ shared/projectPaths.ts
@@ -2,9 +2,9 @@ import * as path from 'path';
 
 const workspaceRoot = path.resolve(__dirname, '../../../');
 
-export const fieldsDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Fields.ods');
-export const valuesDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Values.ods');
-export const elementsDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Elements.ods');
+export const fieldsDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Fields.fods');
+export const valuesDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Values.fods');
+export const elementsDescriptionPath = path.join(workspaceRoot, 'CSV description', 'CSV Elements.fods');
 
 export const readmeBaseFilePath = path.resolve(workspaceRoot, 'readmeBase.md');
 export const readmeOutputFilePath = path.resolve(workspaceRoot, 'README.md');
